4129f3f8a4b7cd9c9e77094148f632f4bbd6fec9
Yuliia <yuliapysarenko@gmail.com>
2023-09-18 16:30:32 +0300

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in proc
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: '/magic-portal/',
     server:
     {
         host: true,
